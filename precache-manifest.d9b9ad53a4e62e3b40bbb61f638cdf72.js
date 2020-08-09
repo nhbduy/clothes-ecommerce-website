e661186edafaf6b6c0bb12c9547e49b19fd19509
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2020-08-09 17:53:30 +0700

    Updates

---
+++ precache-manifest.d9b9ad53a4e62e3b40bbb61f638cdf72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5456579bdf09a774a80f90fc3c67c6ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a29996a6f061475b30ee",
+    "url": "/static/css/main.def64c16.chunk.css"
+  },
+  {
+    "revision": "6714561e28451d9fac04",
+    "url": "/static/js/2.a8ae1a4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a8ae1a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a29996a6f061475b30ee",
+    "url": "/static/js/main.d4b72e14.chunk.js"
+  },
+  {
+    "revision": "b8a2cb4e2336b5b2d30c",
+    "url": "/static/js/runtime-main.61edcef3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clothes-ecommerce-website/precache-manifest.ab3cc4a671bd73479ca63086bdef0c09.js"
+  "/precache-manifest.d9b9ad53a4e62e3b40bbb61f638cdf72.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clothes-ecommerce-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
