b0eab7ad4672b6212c9625609549c9135f9ef66b
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2020-08-09 14:42:36 +0700

    Updates

---
+++ precache-manifest.daa16898d06cca0a09f0d5260313ea95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611b1cbe1b12692cfe556cdd7ac5c44f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f244f1d6508ce92138e",
+    "url": "/static/css/main.def64c16.chunk.css"
+  },
+  {
+    "revision": "6714561e28451d9fac04",
+    "url": "/static/js/2.a8ae1a4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a8ae1a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f244f1d6508ce92138e",
+    "url": "/static/js/main.263020c6.chunk.js"
+  },
+  {
+    "revision": "b8a2cb4e2336b5b2d30c",
+    "url": "/static/js/runtime-main.61edcef3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
